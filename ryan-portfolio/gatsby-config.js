2b22f23973e322e10647022f7ff954aef071e99f
Ryan <rydowd@gmail.com>
2020-01-18 17:36:49 -0500

    updated site metadata from default to a more appropriate description

--- ryan-portfolio/gatsby-config.js
+++ ryan-portfolio/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Dowd`,
+    description: `This site contains information about Ryan and his past and upcoming projects.`,
+    author: `Ryan Dowd`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
